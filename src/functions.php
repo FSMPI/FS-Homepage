cf3834e4089bcd5ba59e2ad93e61d24e925ca72c
mfraas64 <apply.whitespace>
2018-08-17 01:52:49 +0200

    Implemented auto generation of Header Menu

--- src/functions.php
+++ src/functions.php
@@ -5,5 +5,5 @@ require get_template_directory() . '/inc/function-admin.php';
 #require get_template_directory() . '/inc/walker.php';
 #require get_template_directory() . '/inc/walker-mobile.php';
 require get_template_directory() . '/inc/enqueue.php';
-#require get_template_directory() . '/inc/theme-support.php';
+require get_template_directory() . '/inc/theme-support.php';
 #require get_template_directory() . '/inc/custom-post-types.php';
